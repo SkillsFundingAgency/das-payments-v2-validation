298b7baaf37bf2ece514c1d0a0d47b8d91a526f7
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-05-01 11:07:55 +0100

    Small refactoring to seperate getting the list of suitable learners from the rest of the SQL Added a menu to choose the list of learners

---
+++ src/SFA.DAS.Payments.Verification/Constants/Inclusions.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SFA.DAS.Payments.Verification.Constants
+{
+    enum Inclusions
+    {
+        [Description("BasicDayACT2")]
+        Act2BasicDay,
+    }
+}
