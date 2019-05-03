4d0cff0e7e579647cc8e2b0d8a47280c55ba6d29
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-05-03 09:41:55 +0100

    Added scripts to isolate ACT2 learners with refunds

--- src/SFA.DAS.Payments.Verification/SQL/Inclusions/V2/Act2Refund.sql
+++ src/SFA.DAS.Payments.Verification/SQL/Inclusions/V2/Act2Refund.sql
@@ -9,15 +9,9 @@ IF OBJECT_ID('tempdb..#Payments') IS NULL
 BEGIN
 	WITH InitialPayments AS (
 
-		SELECT Uln, LearnRefNumber, Ukprn, PriceEpisodeIdentifier, StandardCode, ProgrammeType, 
-			FrameworkCode, PathwayCode, P.DeliveryMonth, R.CollectionPeriodMonth, R.TransactionType,
-			AmountDue, SfaContributionPercentage, LearnAimRef, FundingSource, Amount, ApprenticeshipContractType
-
-		FROM [DAS_PeriodEnd].PaymentsDue.RequiredPayments R
-		JOIN [DAS_PeriodEnd].Payments.Payments P
-			ON R.Id = P.RequiredPaymentId
-
-		WHERE R.CollectionPeriodName LIKE '1819-R%'
+		SELECT [LearnerUln] [Uln], *
+		FROM [SFA.DAS.Payments.Database].[Payments2].[Payment]
+		WHERE AcademicYear = 1819
 	)
 
 	SELECT * INTO #Payments
@@ -30,7 +24,7 @@ IF OBJECT_ID('tempdb..#Act2Payments') IS NULL
 BEGIN
 	WITH Act2Payments AS (
 		SELECT * FROM #Payments R
-		WHERE ApprenticeshipContractType = 2
+		WHERE ContractType = 2
 		AND SfaContributionPercentage = 0.9
 		
 		AND NOT EXISTS (
@@ -39,7 +33,7 @@ BEGIN
 			WHERE R1.Uln = R.Uln 
 			-- Exclude ULNs that have every been ACT1
 			-- Exclude ULNs that have different sfa contrib 
-			AND (R1.ApprenticeshipContractType = 1 OR
+			AND (R1.ContractType = 1 OR
 				R1.SfaContributionPercentage != 0.9)
 		)
 	)
@@ -79,10 +73,10 @@ BEGIN
 			SELECT *
 			FROM #OnProgAct2Payments R3
 			WHERE R3.Uln = R.Uln
-			AND (COALESCE(R.StandardCode, 0) != COALESCE(R3.StandardCode, 0)
-				OR COALESCE(R.ProgrammeType, 0) != COALESCE(R3.ProgrammeType, 0)
-				OR COALESCE(R.FrameworkCode, 0) != COALESCE(R3.FrameworkCode, 0)
-				OR COALESCE(R.PathwayCode, 0) != COALESCE(R3.PathwayCode, 0)
+			AND (COALESCE(R.LearningAimStandardCode, 0) != COALESCE(R3.LearningAimStandardCode, 0)
+				OR COALESCE(R.LearningAimProgrammeType, 0) != COALESCE(R3.LearningAimProgrammeType, 0)
+				OR COALESCE(R.LearningAimFrameworkCode, 0) != COALESCE(R3.LearningAimFrameworkCode, 0)
+				OR COALESCE(R.LearningAimPathwayCode, 0) != COALESCE(R3.LearningAimPathwayCode, 0)
 			)
 		)
 	)
