0a7984bb47342d4955423ac9b2984fe22c9a4586
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-07-06 14:17:41 +0100

    Fixed period error :)

--- src/SFA.DAS.Payments.Migration/Constants/CollectionPeriods.cs
+++ src/SFA.DAS.Payments.Migration/Constants/CollectionPeriods.cs
@@ -8,16 +8,16 @@ namespace SFA.DAS.Payments.Migration.Constants
         public static Dictionary<int, DateTime> CollectionPeriodDates = new Dictionary<int, DateTime>
         {
             {1, new DateTime(2018, 9, 6, 22, 00, 00) },
-            {1, new DateTime(2018, 10, 4, 22, 00, 00) },
-            {1, new DateTime(2018, 11, 6, 22, 00, 00) },
-            {1, new DateTime(2018, 12, 6, 22, 00, 00) },
-            {1, new DateTime(2019, 1, 7, 22, 00, 00) },
-            {1, new DateTime(2019, 2, 6, 22, 00, 00) },
-            {1, new DateTime(2019, 3, 6, 22, 00, 00) },
-            {1, new DateTime(2019, 4, 4, 22, 00, 00) },
-            {1, new DateTime(2019, 5, 7, 22, 00, 00) },
-            {1, new DateTime(2019, 6, 6, 22, 00, 00) },
-            {1, new DateTime(2019, 7, 4, 22, 00, 00) },
+            {2, new DateTime(2018, 10, 4, 22, 00, 00) },
+            {3, new DateTime(2018, 11, 6, 22, 00, 00) },
+            {4, new DateTime(2018, 12, 6, 22, 00, 00) },
+            {5, new DateTime(2019, 1, 7, 22, 00, 00) },
+            {6, new DateTime(2019, 2, 6, 22, 00, 00) },
+            {7, new DateTime(2019, 3, 6, 22, 00, 00) },
+            {8, new DateTime(2019, 4, 4, 22, 00, 00) },
+            {9, new DateTime(2019, 5, 7, 22, 00, 00) },
+            {10, new DateTime(2019, 6, 6, 22, 00, 00) },
+            {11, new DateTime(2019, 7, 4, 22, 00, 00) },
         };
     }
 }
