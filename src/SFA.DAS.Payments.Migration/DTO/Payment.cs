0c3d31f24cb7c491c12e4d5148b546885d36b109
Carl Berisford-Murray <carl@techcellence.org.uk>
2019-08-05 12:13:31 +0100

    Migrate paymetns from V1 to V2

---
+++ src/SFA.DAS.Payments.Migration/DTO/Payment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SFA.DAS.Payments.Migration.DTO
+{
+    internal class Payment 
+    {
+        public Guid EventId { get; set; } = Guid.NewGuid();
+        public Guid EarningEventId { get; set; } = Guid.Empty;
+        public Guid FundingSourceEventId { get; set; } = Guid.Empty;
+        public DateTimeOffset EventTime { get; set; } = DateTime.Now;
+        public long JobId { get; set; } = 0;
+
+
+
+        public long LearnerUln { get; set; }
+        public long? ApprenticeshipId { get; set; }
+        public long? AccountId { get; set; }
+        public string LearnerReferenceNumber { get; set; }
+        public long Ukprn { get; set; }
+        public DateTime IlrSubmissionDateTime { get; set; }
+        public string PriceEpisodeIdentifier { get; set; }
+
+
+        public int LearningAimStandardCode { get; set; }
+        public int LearningAimProgrammeType { get; set; }
+        public int LearningAimFrameworkCode { get; set; }
+        public int LearningAimPathwayCode { get; set; }
+
+
+
+        public byte ContractType { get; set; }
+        public string LearningAimReference { get; set; }
+        public byte CollectionPeriod { get; set; }
+        public byte TransactionType { get; set; }
+        public decimal SfaContributionPercentage { get; set; }
+        
+        public string LearningAimFundingLineType { get; set; }
+        public byte DeliveryPeriod { get; set; }
+        public short AcademicYear { get; set; }
+        public byte FundingSource { get; set; }
+        public decimal Amount { get; set; }
+
+        public long? TransferSenderAccountId { get; set; }
+        public DateTimeOffset CreationDate { get; set; } = DateTime.Now;
+
+        public DateTime EarningsStartDate { get; set; } = DateTime.Now;
+        public int EarningsCompletionStatus { get; set; } = 0;
+        public int EarningsNumberOfInstalments { get; set; } = 0;
+    }
+}
