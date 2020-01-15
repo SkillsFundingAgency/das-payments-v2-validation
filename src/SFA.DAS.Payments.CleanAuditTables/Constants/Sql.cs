3b5341828be811c7e660392d652290479548132a
Carl Berisford-Murray <carl@techcellence.org.uk>
2020-01-15 14:38:29 +0000

    Added exe to clean audit tables

---
+++ src/SFA.DAS.Payments.CleanAuditTables/Constants/Sql.cs
@@ -0,0 +1,74 @@
+﻿namespace SFA.DAS.Payments.CleanAuditTables.Constants
+{
+    static class Sql
+    {
+        public const string CleanAuditForPeriod = @"
+
+DELETE Payments2.EarningEventPeriod
+WHERE EarningEventId IN (
+	SELECT EventId FROM Payments2.EarningEvent EE
+	WHERE EE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	AND CollectionPeriod = @collectionPeriod
+)
+
+
+
+DELETE Payments2.EarningEventPriceEpisode
+WHERE EarningEventId IN (
+	SELECT EventId FROM Payments2.EarningEvent EE
+	WHERE EE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	AND CollectionPeriod = @collectionPeriod
+)
+
+DELETE Payments2.EarningEvent
+WHERE JobId NOT IN (
+	SELECT DcJobId FROM Payments2.LatestSuccessfulJobs
+)
+AND CollectionPeriod = @collectionPeriod
+
+
+DELETE Payments2.RequiredPaymentEvent
+WHERE JobId NOT IN (
+	SELECT DcJobId FROM Payments2.LatestSuccessfulJobs
+)
+AND CollectionPeriod = @collectionPeriod
+
+
+DELETE Payments2.DataLockEventNonPayablePeriodFailures
+WHERE DataLockEventNonPayablePeriodId IN (
+	SELECT DataLockEventNonPayablePeriodId FROM Payments2.DataLockEventNonPayablePeriod
+	WHERE DataLockEventId IN (
+		SELECT EventId FROM Payments2.DataLockEvent DLE
+		WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+		AND CollectionPeriod = @collectionPeriod
+	)
+)
+
+DELETE Payments2.DataLockEventNonPayablePeriod
+WHERE DataLockEventId IN (
+	SELECT EventId FROM Payments2.DataLockEvent DLE
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	AND CollectionPeriod = @collectionPeriod
+)
+
+DELETE Payments2.DataLockEventPayablePeriod
+WHERE DataLockEventId IN (
+	SELECT EventId FROM Payments2.DataLockEvent DLE
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	AND CollectionPeriod = @collectionPeriod
+)
+
+DELETE Payments2.DataLockEventPriceEpisode
+WHERE DataLockEventId IN (
+	SELECT EventId FROM Payments2.DataLockEvent DLE
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	AND CollectionPeriod = @collectionPeriod
+)
+
+DELETE Payments2.DataLockEvent
+WHERE JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+AND CollectionPeriod = @collectionPeriod
+
+";
+    }
+}
---
+++ src/SFA.DAS.Payments.CleanAuditTables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+using Dapper;
+using SFA.DAS.Payments.CleanAuditTables.Constants;
+
+namespace SFA.DAS.Payments.CleanAuditTables
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var collectionPeriod = await GetPeriod();
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["V2"].ConnectionString))
+            {
+                await connection.ExecuteAsync(Sql.CleanAuditForPeriod, new {collectionPeriod});
+            }
+
+            await Log("Completed - press enter to quit");
+            Console.ReadLine();
+        }
+
+        static async Task Log(string message)
+        {
+            using (var file = File.AppendText("log.txt"))
+            {
+                await file.WriteLineAsync(message);
+            }
+            Console.WriteLine(message);
+        }
+
+        private static async Task<int> GetPeriod()
+        {
+            while (true)
+            {
+                await Log("");
+                await Log("Please enter the collection period: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 or 14");
+                var chosenPeriod = Console.ReadLine();
+                if (!int.TryParse(chosenPeriod, out var collectionPeriod) || collectionPeriod < 1 || collectionPeriod > 14)
+                {
+                    await Log($"Invalid collection period: '{chosenPeriod}'.");
+                    continue;
+                }
+
+                return collectionPeriod;
+            }
+        }
+    }
+}
