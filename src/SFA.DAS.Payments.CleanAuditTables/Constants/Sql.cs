841f02bfd3dcdf7379024246a95b011a7c9c6398
Carl Berisford-Murray <carl@techcellence.org.uk>
2020-01-15 17:36:18 +0000

    Using a temp table for the job ids

--- src/SFA.DAS.Payments.CleanAuditTables/Constants/Sql.cs
+++ src/SFA.DAS.Payments.CleanAuditTables/Constants/Sql.cs
@@ -10,43 +10,40 @@ AND DATEADD(hour, 2, StartTime) > GETDATE()
 ";
 
         public const string CleanAuditForPeriod = @"
+SELECT DcJobId INTO #JobIds FROM Payments2.LatestSuccessfulJobs
 
 DELETE Payments2.EarningEventPeriod
 WHERE EarningEventId IN (
 	SELECT EventId FROM Payments2.EarningEvent EE
-	WHERE EE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	WHERE EE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 	AND CollectionPeriod = @collectionPeriod
 )
-
-
 
 DELETE Payments2.EarningEventPriceEpisode
 WHERE EarningEventId IN (
 	SELECT EventId FROM Payments2.EarningEvent EE
-	WHERE EE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	WHERE EE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 	AND CollectionPeriod = @collectionPeriod
 )
 
 DELETE Payments2.EarningEvent
 WHERE JobId NOT IN (
-	SELECT DcJobId FROM Payments2.LatestSuccessfulJobs
+	SELECT DcJobId FROM #JobIds
 )
 AND CollectionPeriod = @collectionPeriod
-
 
 DELETE Payments2.RequiredPaymentEvent
 WHERE JobId NOT IN (
-	SELECT DcJobId FROM Payments2.LatestSuccessfulJobs
+	SELECT DcJobId FROM #JobIds
 )
 AND CollectionPeriod = @collectionPeriod
-
 
 DELETE Payments2.DataLockEventNonPayablePeriodFailures
 WHERE DataLockEventNonPayablePeriodId IN (
 	SELECT DataLockEventNonPayablePeriodId FROM Payments2.DataLockEventNonPayablePeriod
 	WHERE DataLockEventId IN (
 		SELECT EventId FROM Payments2.DataLockEvent DLE
-		WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+		WHERE DLE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 		AND CollectionPeriod = @collectionPeriod
 	)
 )
@@ -54,26 +51,26 @@ WHERE DataLockEventNonPayablePeriodId IN (
 DELETE Payments2.DataLockEventNonPayablePeriod
 WHERE DataLockEventId IN (
 	SELECT EventId FROM Payments2.DataLockEvent DLE
-	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 	AND CollectionPeriod = @collectionPeriod
 )
 
 DELETE Payments2.DataLockEventPayablePeriod
 WHERE DataLockEventId IN (
 	SELECT EventId FROM Payments2.DataLockEvent DLE
-	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 	AND CollectionPeriod = @collectionPeriod
 )
 
 DELETE Payments2.DataLockEventPriceEpisode
 WHERE DataLockEventId IN (
 	SELECT EventId FROM Payments2.DataLockEvent DLE
-	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+	WHERE DLE.JobId NOT IN (SELECT DcJobId FROM #JobIds)
 	AND CollectionPeriod = @collectionPeriod
 )
 
 DELETE Payments2.DataLockEvent
-WHERE JobId NOT IN (SELECT DcJobId FROM Payments2.LatestSuccessfulJobs)
+WHERE JobId NOT IN (SELECT DcJobId FROM #JobIds)
 AND CollectionPeriod = @collectionPeriod
 
 ";
